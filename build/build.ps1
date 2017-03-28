8ea5a704df9d9f5e2711b4cd1d1c0bac4804dbd3
Alexandr Povar <apo@sitecore.net>
2017-03-28 10:49:28 +0300

    Improve archive packing

--- build/build.ps1
+++ build/build.ps1
@@ -1,9 +1,8 @@
 param(
-    [Parameter(Mandatory=$false)][string]$version = "1.0.0",
+    [Parameter(Mandatory=$false)][string]$version = "1.0.0.0",
     [Parameter(Mandatory=$false)][string]$config="Debug",
     [Parameter(Mandatory=$false)][switch]$skipBuild
 )
-
 
 function Get-MSBuildExe() {
     $path = .\vswhere.exe -latest -products * -requires Microsoft.Component.MSBuild -property installationPath
@@ -17,6 +16,8 @@ function Get-MSBuildExe() {
     return $null
 }
 
+$projectName = "AlexPovar.RiderCleanupFile"
+
 $msBuildPath = Get-MSBuildExe;
 if(-not $msBuildPath) {
     Write-Host "Unable to locate MSBuild" -ForegroundColor "Red"
@@ -25,16 +26,16 @@ if(-not $msBuildPath) {
 
 Write-Host "Detected MSBuild: $msBuildPath" -ForegroundColor "Gray"
 
-& .\nuget.exe restore ..\code\RiderCleanupFile.sln
+& .\nuget.exe restore "..\code\$projectName.sln"
 
 if(-NOT $skipBuild.IsPresent){
-    & $msBuildPath "..\code\RiderCleanupFile.sln" "/t:Build" "/p:Configuration=$config"
+    & $msBuildPath "..\code\$projectName.sln" "/t:Build" "/p:Configuration=$config" "/verbosity:minimal"
 }
 
 # Pack Rider plugin
 $tmpDirPath = ".\temp"
 $pluginDirPath = Join-Path $tmpDirPath "RiderCleanupFile"
-$outputDir = "..\artifacts"
+$outputDir = ".\artifacts"
 
 if(Test-Path $tmpDirPath) {
     Remove-Item -Recurse -Force -Path $tmpDirPath 
@@ -42,11 +43,8 @@ if(Test-Path $tmpDirPath) {
 
 New-Item -ItemType Directory -Force -Path $pluginDirPath |Out-Null
 New-Item -ItemType Directory -Force -Path (Join-Path $pluginDirPath "META-INF") |Out-Null
-if(-not (Test-Path $outputDir)) {
-    New-Item -ItemType Directory -Force -Path $outputDir
-}
 
-& .\nuget.exe pack "AlexPovar.RiderCleanupFile.nuspec" -BasePath "..\code\AlexPovar.RiderCleanupFile\bin\$config\" -OutputDirectory $pluginDirPath -Version $version
+& .\nuget.exe pack "$projectName.nuspec" -BasePath "..\code\$projectName\bin\$config\" -OutputDirectory $pluginDirPath -Version $version
 
 $ideaPluginMetaPath = Join-Path $pluginDirPath "META-INF\plugin.xml"
 Copy-Item -Path ".\idea-plugin.xml" -Destination $ideaPluginMetaPath
@@ -55,5 +53,11 @@ $pluginMetaDoc = [xml] (Get-Content $ideaPluginMetaPath)
 Select-Xml -xml $pluginMetaDoc -XPath //idea-plugin/version | ForEach-Object { $_.Node.'#text' = "$version" }
 $pluginMetaDoc.Save($ideaPluginMetaPath)
 
-Compress-Archive -Path $pluginDirPath -DestinationPath "$outputDir\RiderCleanupFile.$version.zip" -Force
-Remove-Item -Recurse -Force -Path $tmpDirPath 
+Push-Location $tmpDirPath
+..\7za.exe a "..\artifacts\RiderCleanupFile.$version.zip" "RiderCleanupFile"
+Pop-Location
+
+#Add-Type -Assembly "System.IO.Compression.FileSystem" ;
+#[System.IO.Compression.ZipFile]::CreateFromDirectory($pluginDirPath,  "$outputDir\RiderCleanupFile.$version.zip") 
+#Compress-Archive -Path $pluginDirPath -DestinationPath "$outputDir\RiderCleanupFile.$version.zip" -Force
+#Remove-Item -Recurse -Force -Path $tmpDirPath 
